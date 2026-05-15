0a447ddc732a746c4ccd1ab7296f2b60a7626f4c
abas0255 <abas.riz2005@gmail.com>
2026-05-15 18:17:10 +0800

    Added age verification and updated DFD architecture

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template
 import sqlite3
 import uuid
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -11,6 +12,7 @@ def home():
 @app.route('/book-ticket', methods=['POST'])
 def book_ticket():
 
+    # P1 Booking Form Input
     full_name = request.form.get('full_name')
     email = request.form.get('email')
     phone = request.form.get('phone')
@@ -18,8 +20,24 @@ def book_ticket():
     ic_number = request.form.get('ic_number')
     payment_reference = request.form.get('payment_reference')
 
+    # P2 Age Verification
+    dob = datetime.strptime(date_of_birth, '%Y-%m-%d')
+    today = datetime.today()
+
+    age = today.year - dob.year - (
+        (today.month, today.day) < (dob.month, dob.day)
+    )
+
+    if age < 18:
+        return """
+        <h1>Booking Failed</h1>
+        <p>User must be 18 years old and above.</p>
+        <a href="/">Back to Booking Page</a>
+        """
+    # P3 Ticket Generator
     ticket_id = str(uuid.uuid4())[:8]
 
+    # Database Connection
     connection = sqlite3.connect('tickets.db')
     cursor = connection.cursor()
 
@@ -49,6 +67,7 @@ def book_ticket():
     return f"""
     <h1>Ticket Booked Successfully</h1>
     <p>Ticket ID: {ticket_id}</p>
+    <p>Age Verified: {age}</p>
     <a href="/">Back to Booking Page</a>
     """
 
